cfce25497c20358407521a4570c1d2508c6df6a3
Ievgenii <bijibnui88@gmail.com>
2020-11-01 14:14:19 +0200

    added index.html

---
+++ build/precache-manifest.a6b8a5ce2d06ebeb03a76efb13a83264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c18619d641460309570a8dd6c70f191",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "fea2b0d21d10e5495ef5",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.f25fd48f.chunk.css"
+  },
+  {
+    "revision": "ece54d8e3fbd45893c11",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d1e28114.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d1e28114.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fea2b0d21d10e5495ef5",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.c5d48860.chunk.js"
+  },
+  {
+    "revision": "21fc850b4b86db889715",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.0c890f5e.js"
+  }
+]);
